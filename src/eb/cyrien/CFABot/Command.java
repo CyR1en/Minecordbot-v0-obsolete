ed2738c94c8daaea450ab939221517cb809d2770
CyR1en <ethanbacurio@yahoo.com>
2016-08-12 13:14:48 -0700

    interface to abstract class -> implemented hasPermission();

--- src/eb/cyrien/CFABot/Command.java
+++ src/eb/cyrien/CFABot/Command.java
@@ -2,13 +2,29 @@ package eb.cyrien.CFABot;
 
 import net.dv8tion.jda.events.message.MessageReceivedEvent;
 
-public interface Command {
+import java.util.ArrayList;
 
-    public boolean called(String[] args, MessageReceivedEvent e);
+public abstract class Command {
 
-    public void action(String[] args, MessageReceivedEvent e);
+    public abstract boolean called(String[] args, MessageReceivedEvent e);
 
-    public String help();
+    public abstract void action(String[] args, MessageReceivedEvent e);
 
-    public void executed(boolean success, MessageReceivedEvent e);
+    public abstract String help();
+
+    public abstract void executed(boolean success, MessageReceivedEvent e);
+
+    public String noPermMessage() {
+        return "```css\n[You do not have permission]\n```";
+    }
+
+    public static boolean hasPermission(String userID) {
+        ArrayList<String> wl = Main.botConfig.WHITELIST;
+        for (String s : wl) {
+            if(s.equals(userID))
+                return true;
+        }
+        return false;
+    }
+
 }
