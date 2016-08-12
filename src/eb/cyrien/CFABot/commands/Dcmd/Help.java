4f4a7dfeca3f5afc6054dbe2e9dd897e7904bf00
CyR1en <ethanbacurio@yahoo.com>
2016-08-12 13:19:41 -0700

    initial commit for Help command in discord
    Lists all the commands and show usage.

---
+++ src/eb/cyrien/CFABot/commands/Dcmd/Help.java
@@ -0,0 +1,36 @@
+package eb.cyrien.CFABot.commands.Dcmd;
+
+import eb.cyrien.CFABot.Command;
+import eb.cyrien.CFABot.Main;
+import net.dv8tion.jda.events.message.MessageReceivedEvent;
+
+import java.util.Map;
+
+public class Help extends Command {
+
+    public static final String HELP = "USAGE: " + Main.botConfig.COMMAND_EXECUTOR + "help";
+
+    @Override
+    public boolean called(String[] args, MessageReceivedEvent e) {
+        return true;
+    }
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent e) {
+        String cmdExec = Main.botConfig.COMMAND_EXECUTOR;
+        String out = "```\n";
+        for(Map.Entry<String, Command> entry : Main.commands.entrySet())
+            out += cmdExec + entry.getKey() + " - " + entry.getValue().help() +  "\n";
+        out += "```";
+        e.getTextChannel().sendMessage(out);
+    }
+
+    @Override
+    public String help() {
+        return HELP;
+    }
+
+    @Override
+    public void executed(boolean success, MessageReceivedEvent e) {
+    }
+}
