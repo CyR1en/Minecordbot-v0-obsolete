e2390cbdf61b9f9b07bc665735f1d921cfa048ca
CyR1en <ethanbacurio@yahoo.com>
2016-08-29 05:14:15 -0700

    something new

---
+++ src/eb/cyrien/MineCordBot/commands/Dcmd/Mcmd.java
@@ -0,0 +1,38 @@
+package eb.cyrien.MineCordBot.commands.Dcmd;
+
+import eb.cyrien.MineCordBot.Command;
+import eb.cyrien.MineCordBot.Main;
+import net.dv8tion.jda.events.message.MessageReceivedEvent;
+import org.bukkit.Bukkit;
+
+public class Mcmd extends Command {
+
+    private final String HELP = Main.botConfig.COMMAND_EXECUTOR + "mcmd <command>";
+    private final String DESCRIPTION = "Do Minecraft commands from discord";
+
+    public Mcmd() {
+        setDescription(DESCRIPTION);
+        setUsage(HELP);
+    }
+
+    @Override
+    public boolean called(String[] args, MessageReceivedEvent e) {
+        if (!hasPermission(e.getAuthor().getId()))
+            if (!e.getAuthor().getId().equals(Main.botConfig.OWNER_ID))
+                return false;
+        return true;
+    }
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent e) {
+        Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), Main.concatenateArgs(0, args).trim());
+    }
+
+    @Override
+    public void executed(boolean success, MessageReceivedEvent e) {
+        if (success)
+            e.getTextChannel().sendMessage(":white_check_mark:");
+        else
+            e.getTextChannel().sendMessage(noPermMessage());
+    }
+}
