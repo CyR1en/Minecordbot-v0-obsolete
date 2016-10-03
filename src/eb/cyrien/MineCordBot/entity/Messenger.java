e3e05c7ef08bffe9601188f05a73b25fa0e24311
CyR1en <ethanbacurio@yahoo.com>
2016-10-03 01:05:04 -0700

    fixed problem where it doesn't send message to MC

--- src/eb/cyrien/MineCordBot/entity/Messenger.java
+++ src/eb/cyrien/MineCordBot/entity/Messenger.java
@@ -6,7 +6,7 @@ import net.dv8tion.jda.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.hooks.ListenerAdapter;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.craftbukkit.v1_9_R2.command.CraftConsoleCommandSender;
+import org.bukkit.craftbukkit.v1_10_R1.command.CraftConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
@@ -25,8 +25,9 @@ public class Messenger extends ListenerAdapter implements Listener {
         if(author == null)
             author = e.getAuthorName();
         String msg = e.getMessage().getContent();
-        for(Player p : Bukkit.getServer().getOnlinePlayers())
-            p.sendMessage(ChatColor.translateAlternateColorCodes('&', Main.botConfig.MESSAGE_PREFIX_MINECRAFT + "&r"+ " ~ [&9&l" + author + "&r]: " + ChatColor.WHITE + msg ));
+        for(Player p : Bukkit.getServer().getOnlinePlayers()) {
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', Main.botConfig.MESSAGE_PREFIX_MINECRAFT + "&r" + " ~ [&9&l" + author + "&r]: " + ChatColor.WHITE + msg));
+        }
         Main.log.info(author+ " > mc: " + msg );
     }
 
@@ -44,7 +45,7 @@ public class Messenger extends ListenerAdapter implements Listener {
     public void onMessageReceived(MessageReceivedEvent event) {
         boolean validChannel = event.getTextChannel().getId().equals(Main.botConfig.BINDED_CHANNEL);
         boolean notSelf = event.getMessage().getAuthor().getId() != event.getJDA().getSelfInfo().getId();
-        boolean notCommand = event.getMessage().toString().startsWith(Main.botConfig.COMMAND_EXECUTOR);
+        boolean notCommand = !event.getMessage().toString().startsWith(Main.botConfig.COMMAND_EXECUTOR);
         if(validChannel && notSelf && notCommand)
             relayToMinecraft(event);
     }
