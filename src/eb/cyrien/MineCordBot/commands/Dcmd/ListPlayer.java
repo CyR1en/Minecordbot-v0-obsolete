8d60f3e378345137260d871aad7c75261c48e8ec
CyR1en <ethanbacurio@yahoo.com>
2016-10-12 23:37:20 -0700

    Added new feature for listing players
    It will auto update the list in a certain interval and check who's on
    the server.

--- src/eb/cyrien/MineCordBot/commands/Dcmd/ListPlayer.java
+++ src/eb/cyrien/MineCordBot/commands/Dcmd/ListPlayer.java
@@ -2,20 +2,27 @@ package eb.cyrien.MineCordBot.commands.Dcmd;
 
 import eb.cyrien.MineCordBot.Command;
 import eb.cyrien.MineCordBot.Main;
-import eb.cyrien.MineCordBot.entity.Messenger;
-import net.dv8tion.jda.entities.TextChannel;
 import net.dv8tion.jda.events.message.MessageReceivedEvent;
+import org.apache.commons.lang.math.NumberUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class ListPlayer extends Command {
 
-    private final String HELP = Main.botConfig.COMMAND_EXECUTOR + "ls";
+    private final String HELP = Main.botConfig.COMMAND_EXECUTOR + "ls \n + ls <update:(true or false)> \n + ls <update:true> <update interval(seconds)>";
     private final String DESCRIPTION = "List all players currently online in your minecraft server";
+
+    private Timer timer;
+
+    private boolean updating;
 
     public ListPlayer() {
         setUsage(HELP);
         setDescription(DESCRIPTION);
+        timer = new Timer();
     }
 
     @Override
@@ -25,25 +32,77 @@ public class ListPlayer extends Command {
 
     @Override
     public void action(String[] args, MessageReceivedEvent e) {
+        String s = generateList(e);
+        if (args.length == 0 || args == null) {
+            sendTyping(.3, e);
+            sendMessageToDiscord(s, e);
+        } else if (args.length > 2) {
+            boolean argumentIsNumber = NumberUtils.isNumber(args[1]);
+            if (args[0].equalsIgnoreCase("update:true") && argumentIsNumber) {
+                String mID = e.getTextChannel().sendMessage(s).getId();
+                String tID = e.getTextChannel().getId();
+                Timer timer = new Timer();
+                updating = true;
+                timer.schedule(new Update(mID, tID, e, Long.parseLong(args[1]) * 1000), 0);
+            } else
+                sendMessageToDiscord(invalidArgsMessage(), e);
+        } else {
+            if (args[0].equalsIgnoreCase("update:true")) {
+                String mID = e.getTextChannel().sendMessage(s).getId();
+                String tID = e.getTextChannel().getId();
+                updating = true;
+                timer.schedule(new Update(mID, tID, e, 5000 ), 0);
+            } else if (args[0].equalsIgnoreCase("update:false")) {
+                updating = false;
+                sendMessageToDiscord(successMessage(), e);
+            }
+            else
+                sendMessageToDiscord(invalidArgsMessage(), e);
+        }
+    }
+
+    private String generateList(MessageReceivedEvent e) {
         String blockFix = "```";
-        TextChannel tc = e.getTextChannel();
-        tc.sendMessage("Online Players in " + Bukkit.getServer().getName());
         int i = 1;
-        String s = blockFix + "\n";
+        String s = "Online Players in " + Bukkit.getServer().getName() + blockFix + "\n";
         if (Bukkit.getServer().getOnlinePlayers().size() == 0 || Bukkit.getServer().getOnlinePlayers() == null)
-            s += "There are no players online";
+            s += "There are no players online. ";
         else
             for (Player p : Bukkit.getServer().getOnlinePlayers()) {
-                s += (i + ". " + p.getName() + "\n");
-                i++;
+                s += (i++ + ". " + p.getName() + "\n");
             }
         s += blockFix;
-        Messenger.sendTyping(4, e);
-        e.getTextChannel().sendMessage(s);
+        return s;
     }
 
     @Override
     public void executed(boolean success, MessageReceivedEvent e) {
         return;
+    }
+
+    public class Update extends TimerTask {
+
+        private String mID;
+        private String tID;
+        private MessageReceivedEvent mE;
+        private long delay;
+
+        public Update(String mID, String tID, MessageReceivedEvent mE, long delay) {
+            this.mID = mID;
+            this.tID = tID;
+            this.mE = mE;
+            this.delay = delay;
+        }
+
+        @Override
+        public void run() {
+            if (updating == false) {
+                return;
+            }
+            else {
+                mE.getJDA().getTextChannelById(tID).getMessageById(mID).updateMessage(generateList(mE));
+                timer.schedule(new Update(mID, tID, mE, delay), delay);
+            }
+        }
     }
 }
