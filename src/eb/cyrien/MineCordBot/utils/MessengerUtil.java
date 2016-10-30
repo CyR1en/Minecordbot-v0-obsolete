ffa4b2c45459ee5fc13410d9b113678b434fe19b
CyR1en <ethanbacurio@yahoo.com>
2016-10-29 19:43:39 -0700

    Added Messenger Util Class.

---
+++ src/eb/cyrien/MineCordBot/utils/MessengerUtil.java
@@ -0,0 +1,37 @@
+package eb.cyrien.MineCordBot.utils;
+
+import eb.cyrien.MineCordBot.Main;
+import net.dv8tion.jda.events.message.MessageReceivedEvent;
+
+public class MessengerUtil {
+
+    public static String concatenateArgs(int startIndex,String[] args) {
+        String concatenatedString = "";
+        for(int i = startIndex; i < args.length; i++)
+            concatenatedString += " " + args[i];
+        concatenatedString.trim();
+        return concatenatedString;
+    }
+
+    public static void sendTyping(double sec, MessageReceivedEvent e) {
+        long startTime = System.currentTimeMillis();
+        long diff = System.currentTimeMillis() - startTime;
+        while(diff < (sec * 1000)) {
+            e.getTextChannel().sendTyping();
+            diff = System.currentTimeMillis() - startTime;
+        }
+    }
+
+    public static void sendMessageToDiscord(String msg) {
+        for(String s : Main.getInstance().getBotConfig().BINDED_CHANNELS)
+            Main.getInstance().jda.getTextChannelById(s).sendMessage(msg);
+    }
+
+    public static void sendMessageToDiscord(String textChannelID, String msg) {
+            Main.getInstance().jda.getTextChannelById(textChannelID).sendMessage(msg);
+    }
+
+    public static void sendMessageToDiscord(String msg, MessageReceivedEvent e) {
+        e.getTextChannel().sendMessage(msg);
+    }
+}
